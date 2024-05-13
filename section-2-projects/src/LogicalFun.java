b97613af78730dbc000d2197019cc88b05f5cff9
Darren Whatford <darrenwhatford@hastingsdirect.com>
2024-05-13 17:04:28 +0100

    Added logical java class update

--- section-2-projects/src/LogicalFun.java
+++ section-2-projects/src/LogicalFun.java
@@ -1,22 +1,22 @@
 public class LogicalFun {
     //psvm shortcut keys to create this...
     public static void main (String[] args) {
-        boolean isRunning = true;
+        boolean isRaining = true;
         boolean isWarm = true;
 
         // && both need to be true
         // || only one need be true
         // ! not equal to, ie flips true/false
 
-        boolean combined = isRunning && isWarm;
+        boolean combined = isRaining && isWarm;
 
         System.out.println("Is it raining and warm? " + combined);
 
-        combined = isRunning || isWarm;
+        combined = isRaining || isWarm;
 
         System.out.println("Is it raining OR warm? " + combined);
 
-        combined = !isRunning;
+        combined = !isRaining;
 
         System.out.println("Is it NOT raining outside? " + combined);
 
