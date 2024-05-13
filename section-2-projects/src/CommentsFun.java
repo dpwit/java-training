0d3f4168d88e5e028459eb8c1eb9a9d16230de65
Darren Whatford <darrenwhatford@hastingsdirect.com>
2024-05-13 11:12:58 +0100

    Added a new comments Java file

---
+++ section-2-projects/src/CommentsFun.java
@@ -0,0 +1,20 @@
+/*
+Use these to
+add multi-line comments
+to your code
+
+Helps describe what should be going on
+*/
+public class CommentsFun {
+    /*
+    main
+    is the entry point to the
+    application.
+     */
+    public static void main (String[] args) {
+        System.out.println("This will print out in the console"); // this will print to the console
+        // Use this for a single line comment
+        // This will not print out.
+
+    } //end main
+}
