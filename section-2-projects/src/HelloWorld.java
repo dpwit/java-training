399994fe679e4d2aad5423f7c105df9aeafe085f
Darren Whatford <darrenwhatford@hastingsdirect.com>
2024-05-13 08:49:49 +0100

    Added new set up files

---
+++ section-2-projects/src/HelloWorld.java
@@ -0,0 +1,13 @@
+//The Java class name must match the file name
+//A Java class can hold data, behaviours or operators
+public class HelloWorld {
+
+    //this is the main method and acts as the entry point to the application.
+    //public means other classes and methods outside this class have full access to it.
+    //static means the main method belongs to the HelloWord class.
+    //public static void main String args are all required.
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello Darren!");
+    }
+}
