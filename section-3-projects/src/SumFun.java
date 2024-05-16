1eb144078657cacd49eeac9990d1949f50390ce7
Darren Whatford <darrenwhatford@hastingsdirect.com>
2024-05-16 09:00:14 +0100

    Added new sum while java class

---
+++ section-3-projects/src/SumFun.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SumFun {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        int sum = 0;
+        int input;
+
+        // priming read
+        System.out.println("Please enter a non-negative integer to sum together,");
+        System.out.println("or enter a negative integer to end the calculation!");
+
+        input = keyboard.nextInt();
+
+        while(input >= 0) {
+            sum += input; //same as sum = sum + input
+            System.out.println("Please enter a non-negative integer to sum together,");
+            System.out.println("or enter a negative integer to end the calculation!");
+
+            input = keyboard.nextInt();
+        } // end while loop
+
+
+        System.out.println("Sum is " + sum);
+    } // end main
+}
