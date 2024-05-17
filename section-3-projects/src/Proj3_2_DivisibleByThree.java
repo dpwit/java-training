6e2ae9eedf7ad268317b0632be53457cb9534f9a
Darren Whatford <darrenwhatford@hastingsdirect.com>
2024-05-17 08:36:19 +0100

    Added a new divisible by three checker java class file

---
+++ section-3-projects/src/Proj3_2_DivisibleByThree.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Proj3_2_DivisibleByThree {
+
+    public static void main(String[] args) {
+        // use selection statement if else and modulus operator
+
+        // Scanner is listening for keyboard inputs to capture and return.
+        Scanner keyboard = new Scanner(System.in);
+
+        int divisibleThree;
+
+        System.out.print("Enter an integer to see if it is divisible by 3...\t");
+        divisibleThree = keyboard.nextInt();
+
+        if(divisibleThree % 3 == 0) {
+            System.out.println("This integer is divisible by 3, well done!");
+        }
+        else {
+            System.out.println("This is integer is NOT divisible by 3, unfortunately!");
+        }
+
+    } // end main
+}
