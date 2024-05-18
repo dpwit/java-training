85c86e2506e7bb3c9b5f235d1d829aca3677a820
Darren Whatford <darrenwhatford@hastingsdirect.com>
2024-05-18 09:05:16 +0100

    Completed project for adding values to an array and doubling

---
+++ section-4-projects/src/Proj4_1_Arrays.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; // needed for keyboard input
+
+public class Proj4_1_Arrays {
+    public static void main(String[] args) {
+        int[] myArray = new int[5]; // creates an array of 5 elements
+
+        Scanner keyboard = new Scanner(System.in);
+
+        // for loop - stores the values from the keyboard entries
+        for(int i = 0; i < myArray.length; i++) {
+            System.out.print("Please enter an integer...\t");
+            myArray[i] = keyboard.nextInt();
+        } // end for
+
+       // for loop to restore the value and * by 2 and prints out
+        for(int i = 0; i < myArray.length; i++) {
+//            int result = myArray[i] * 2; // you can you this method, which may be easier to understand.
+//            System.out.println(result);
+            System.out.println(myArray[i] * 2); // this is a cleaner code method - which makes sense to me.
+        }
+
+    } //end main
+}
