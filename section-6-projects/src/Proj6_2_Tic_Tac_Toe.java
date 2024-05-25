62a556201ccec433a5a19846d2be54c061a3e02b
Darren Whatford <darrenwhatford@hastingsdirect.com>
2024-05-25 08:28:48 +0100

    Set up the structure for the project tic tac toe

--- section-6-projects/src/Proj6_2_Tic_Tac_Toe.java
+++ section-6-projects/src/Proj6_2_Tic_Tac_Toe.java
@@ -1,6 +1,47 @@
+// Tic Tac Toe 'noughts and crosses' game application
+// check out the resource file
+
+// multiple methods to break the code into smaller sizes
+
+
 public class Proj6_2_Tic_Tac_Toe {
     public static void main(String[] args) {
 
     } // end main method
 
-}
+    // this is the game loop
+    public static void runGame() {
+
+    } // end runGame method
+
+    // initialiseGame sets the cells of a 2D array to spaces
+    public static void initialiseGame(String[] gameBoard) {
+
+    } // end initialiseGame method
+
+    // printGameBoard = prints whatever is in the current board game
+    public static void printBoardGame(String[] gameBoard){
+
+    } // end printGameBoard method
+
+    // getUserInput = gets the user input and if valid, sets the current game board accordingly
+    public static void getUserInput(boolean xTurn, String[] gameBoard) {
+
+    } // end getUserInput method
+
+    // cellAlreadyOccupied = checks whether the chosen cell already has a value stored in it, and returns true if the cell is occupied.
+    public static boolean cellAlreadyOccupied(int row, int col, String[] gameBoard) {
+        return false;
+    } // end cellAlready Occupied method
+
+    // getWinner = returns 'X' or 'O' if there is a clear winner or '' a space if there is no winner yet
+    public static String getWinner(String[] gameBoard) {
+        return "";
+    } // enf getWinner method
+
+    // isBoardFull = returns if the board is full or not
+    public static boolean isBoardsFull(String[] gameBoard) {
+        return false;
+    } // end isBoardFull method
+
+} // end class.
