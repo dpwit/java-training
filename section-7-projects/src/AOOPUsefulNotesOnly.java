792201271fa8a729a77272c59c2064ffae3741b5
Darren Whatford <darrenwhatford@hastingsdirect.com>
2024-05-28 08:58:46 +0100

    Added a useful notes file to add to

---
+++ section-7-projects/src/AOOPUsefulNotesOnly.java
@@ -0,0 +1,10 @@
+public class AOOPUsefulNotesOnly {
+    public static void main(String[] args) {
+        // OOP = Object-Oriented Programming
+        // Objects which have behaviours and data
+        // UML = Unified Modelling Language
+
+        // Three principals for OOP
+        // Encapsulation, inheritance and Polymorphism
+    }
+}
