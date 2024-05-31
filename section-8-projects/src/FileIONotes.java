c4e3b6ac8dc27032a32f584a397075d06b39315f
Darren Whatford <darrenwhatford@hastingsdirect.com>
2024-05-31 09:16:19 +0100

    Created new file input class example

---
+++ section-8-projects/src/FileIONotes.java
@@ -0,0 +1,6 @@
+public class FileIONotes {
+    // File I/O = File Input/Output
+    // File Input - reading data from a file (load data) to populate our program
+    // File Output - writing data to a file from our program to use later.
+
+}
---
+++ section-8-projects/src/FileInputFun.java
@@ -0,0 +1,39 @@
+// Exception is just a class
+// You can have more than one Exception to catch different scenarios
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class FileInputFun {
+    public static void main(String[] args) {
+        Scanner infile;
+
+        try {
+            infile = new Scanner(new File("input.txt"));
+            int input;
+            int inputSum = 0;
+
+            while(infile.hasNext()) {
+                input = infile.nextInt();
+                System.out.println(input);
+                inputSum += input; // same as inputSum = inputSum + input.
+            } // end while loop
+
+            System.out.println("The sum of the numbers is: " + inputSum);
+
+            infile.close();
+        }
+        catch(FileNotFoundException ex) { // handles the exception if the specified file is not found.
+            System.out.println("Can't find the file you specified! :(");
+            System.out.println(ex.getMessage());
+        }
+        catch(InputMismatchException ex) {
+            System.out.println("Error in the input file, entry is not a number!");
+        } // end try-catch
+
+
+
+    } // end main method
+} // end FileInputFun class
