596f498fea69def9afe3f22ba3d416af8d29db6f
Darren Whatford <darrenwhatford@hastingsdirect.com>
2024-06-03 09:14:39 +0100

    Added Polymorphism notes

---
+++ section-9-projects/src/AAPolymorphismNotes.java
@@ -0,0 +1,6 @@
+public class AAPolymorphismNotes {
+    // The third primary principle of OOP, Encapsulation, Inheritance and Polymorphism
+    // Best demo'd with references and dynamic memory
+    // Heavily related to and connected with Inheritance.
+
+} // end notes
