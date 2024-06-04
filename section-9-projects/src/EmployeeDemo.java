471537e21fc0e75cf8fe69703d02ea09854cf77b
Darren Whatford <darrenwhatford@hastingsdirect.com>
2024-06-04 16:14:42 +0100

    Updated Employee project task

--- section-9-projects/src/EmployeeDemo.java
+++ section-9-projects/src/EmployeeDemo.java
@@ -1,18 +1,29 @@
+import java.util.Random;
+
 public class EmployeeDemo {
+
+    private enum EmployeeType {
+        MANAGER,
+        WORKER
+
+    } // end enum
+
     public static void main(String[] args) {
         Employee[] employees = new  Employee[5];
 
-        employees[0] = new Manager("Darren", "Whatford", 55);
-        employees[1] = new Worker("Kate", "Whatford", 17);
-        employees[2] = new Manager("Jules", "Whatford", 54);
-        employees[3] = new Worker("Daisy", "Whatford", 24);
-        employees[4] = new Worker("Ed", "Thedog", 49);
+        for(int i = 0; i < 5; i++) {
+            EmployeeType et = getEmployeeType();
+            Employee currentEmployee;
 
-        printEmployess(employees);
+            if(et == EmployeeType.MANAGER) {
+                currentEmployee = new Manager("Darren", "Whatford", 54);
+            } else {
+                currentEmployee = new Worker("Ed", "Whatford", 18);
+            }
 
-    } // end main method
+            employees[i] = currentEmployee;
+        } // end of creating employees
 
-    public static void printEmployess(Employee[] employees) {
         for (Employee employee : employees) {
             System.out.println("First name is " + employee.getFirstName());
             System.out.println("Last name is " + employee.getLastName());
@@ -20,5 +31,24 @@ public class EmployeeDemo {
             System.out.println("Employee says: " + employee.work());
             System.out.println(); // just adds an extra line space.
         }
+
+
+    } // end main method
+
+    public static EmployeeType getEmployeeType() {
+        EmployeeType result;
+            Random rand = new Random();
+            final int UPPER_BOUND = 2; // only 2 values 0 Manager 1 Worker
+            int whichOne = rand.nextInt(UPPER_BOUND);
+
+            if(whichOne == 0) {
+                result = EmployeeType.MANAGER;
+            }
+            else {
+                result = EmployeeType.WORKER;
+            }
+        return result;
     }
+
+
 } // end EmployeeDemo class
